0796ba813da920aa28bd5bee1246561d9570e33e
sarkarsurya456-a11y <sarkarsurya456@gmail.com>
2026-08-08 19:30:47 +0530

    Time: 0 ms (100%), Space: 8.8 MB (63.01%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        int count[26];
+        for (int i = 0; i < sentence.size(); i++) {
+            count[sentence[i] - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count[i] ==0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};

998d27878a19c47a5bc192cdc9b9c2e3f089e956
sarkarsurya456-a11y <sarkarsurya456@gmail.com>
2026-08-08 19:58:55 +0530

    Time: 3 ms (15.8%), Space: 9.3 MB (83.8%) - LeetHub

---
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+       int count[26];
+       for(int i =0;i<s.size();i++){
+        count[s[i]-'a']++;
+       } 
+       int freq=0;
+       for(int i =0;i<26;i++){
+        if(count[i]>0){
+            freq=count[i];
+            break ;
+        }
+       }
+     for(int i = 0; i < 26; i++) {
+            if(count[i] > 0 && count[i] != freq) {
+                return false;
+            }
+        }
+        return true ;
+    }
+};

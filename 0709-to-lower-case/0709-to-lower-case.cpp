b390494fa3ec9868c8d96041cb6a4de95cee30b3
sarkarsurya456-a11y <sarkarsurya456@gmail.com>
2026-08-10 20:10:41 +0530

    Time: 3 ms (0.7%), Space: 8.4 MB (16.29%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
-    string toLowerCase(string s){ 
-        for( int  i =0;i<s.size();i++){
-            if(s[i]>='A' && s[i]<='Z'){
-                s[i]=tolower(s[i]);
+    string change(string s) {
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] >= 'A' && s[i] <= 'Z') {
+                s[i] = tolower(s[i]);
             }
         }
         return s;
+    }
+
+        string toLowerCase(string s) { 
+            return change(s);
+             
     }
 };

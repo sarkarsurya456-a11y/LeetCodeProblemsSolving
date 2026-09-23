a9168e41597fc3ab6fd07e108deb0aac508df77c
sarkarsurya456-a11y <sarkarsurya456@gmail.com>
2026-09-23 16:16:20 +0530

    Time: 0 ms (100%), Space: 8.4 MB (24.23%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+     int solve(int n,vector<int>& dp) {
+         if (n == 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+
+        }
+
+        
+         dp[n]= solve(n-1,dp)+ 
+                solve(n-2,dp)+ 
+                solve(n-3,dp);
+
+         return dp[n];
+    }
+
+public:
+    int tribonacci(int n) {
+        vector<int>dp(n+1,-1);
+    
+        return solve(n,dp);
+    }
+};
